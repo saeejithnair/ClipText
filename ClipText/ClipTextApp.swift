6cc3f04ae3fb94589e900d2b1378a292a72ab27b
Saeejith Nair <saeejithn@gmail.com>
2025-01-25 22:47:26 -0500

    Initial Commit

---
+++ ClipText/ClipTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipTextApp.swift
+//  ClipText
+//
+//  Created by Saeejith Nair on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct ClipTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
